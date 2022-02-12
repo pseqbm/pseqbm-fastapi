dd9a7e2865d462e57d5c5ee1328e7a2c95ac9994
pseqbm <92953794+pseqbm@users.noreply.github.com>
2022-02-11 18:00:09 -0800

    Delete tests/api directory

--- tests/api/conftest.py
+++
@@ -1,2 +0,0 @@
-#test_users.py do not have access to this conftest.py but anything within api can have access to it
-#this case we limit the scope fixtures 
--- tests/api/test_something.py
+++
@@ -1,1 +0,0 @@
-#because this test within test package it still has access to conftest.py outside of the package
